dde5f341d1ff2022c33b70a8864c285a3dd0f8cd
Ernar Gabbasov <ernar2272@gmail.com>
2024-08-04 21:32:33 +0600

    euphoria-web-site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Shop from './pages/Shop';
+import Men from './pages/Men';
+import Women from './pages/Women';
+import Combos from './pages/Combos';
+import Joggers from './pages/Joggers';
+import SignInPage from './pages/SignInPage';
+import ProductDetailPage from './pages/ProductDetailPage';
+// import './styles/index.scss';
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem("userToken");
+    setToken(storedToken);
+  },
+);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignInPage token={token} setToken={setToken}/>} />
+        <Route path="/shop" element={token ? <Shop /> : <Navigate to="/" />} />
+        <Route path="/men" element={<Men />} />
+        <Route path="/women" element={<Women />} />
+        <Route path="/product/:productId" element={<ProductDetailPage />} />
+        <Route path="/combos" element={<Combos />} />
+        <Route path="/joggers" element={<Joggers />} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
